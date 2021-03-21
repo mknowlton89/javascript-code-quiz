92caf681e9726825adbcddf9463a1943044e9c75
Michael Knowlton <mknowlton89@gmail.com>
2021-03-21 15:28:58 -0400

    Started building the data structure for the question bank, and began dynamically adding the questions and answer options to the quiz section.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,55 @@
-let timeLeft = 100;
+// JS Variables
+let timeLeft = 10000;
+let questionNumber = 0;
+let quizQuestions = [
+    {
+        question: "What Javascript data type enables you to store a list of similar items?",
+        correctAnswer: "C",
+        answerA: "A: String",
+        answerB: "B: Number",
+        answerC: "C: Array",
+        answerD: "D: Boolean"
+    },
+    {
+        question: "What are the following called in Javascript? '=, >, <, !='",
+        correctAnswer: "A",
+        answerA: "A: Operators",
+        answerB: "B: Functions",
+        answerC: "C: Variables",
+        answerD: "D: Tags"
+    }
+]
 
+// DOM Variables
 let emptyState = document.getElementById("empty-state");
 let quiz = document.getElementById("quiz");
 let timeEl = document.getElementById("timer");
 let gameOverEl = document.getElementById("game-over");
+let questionTitle = document.getElementById("question-title");
+let buttonA = document.getElementById("a");
+let buttonB = document.getElementById("b");
+let buttonC = document.getElementById("c");
+let buttonD = document.getElementById("d");
 
+// Function Declarations
 function startQuiz(){
     // emptyState.setAttribute("style", "visibility: hidden");
     emptyState.classList.toggle("hidden");
     // quiz.setAttribute("style", "visibility: visible");
     quiz.classList.toggle("hidden");
+
+    let question = quizQuestions[questionNumber].question;
+    let answerA = quizQuestions[questionNumber].answerA;
+    let answerB = quizQuestions[questionNumber].answerB;
+    let answerC = quizQuestions[questionNumber].answerC;
+    let answerD = quizQuestions[questionNumber].answerD;
+
+    questionTitle.innerHTML = question;
+    buttonA.innerHTML = answerA;
+    buttonB.innerHTML = answerB;
+    buttonC.innerHTML = answerC;
+    buttonD.innerHTML = answerD;
+
 }
 
 
@@ -35,6 +75,5 @@ function countdownTimer() {
     }, 1000);
   }
 
-
-
+// Event Listeners
 document.getElementById("start-quiz").addEventListener("click", countdownTimer);
