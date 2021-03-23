0ae68bba760528bf0ab1abd80d7a556668b9c465
Michael Knowlton <mknowlton89@gmail.com>
2021-03-22 20:43:20 -0400

    I was able to figure out how to sort the array of objects by score, but now, I need to figure out how to sort the array on page load and then print the winners in order.

--- assets/leaderboard.js
+++ assets/leaderboard.js
@@ -1,14 +1,40 @@
 let userArray = JSON.parse(localStorage.getItem("userList"));
 
+console.log(userArray);
+
+// userArray.sort((b, a) => a.score > b.score);
+
+console.log(userArray);
+
 let listEl = document.getElementById("list");
 
-for (let i = 0; i < userArray.length; i++) {
-    let initials = userArray[i].initials;
-    let score = userArray[i].score;
+function printWinners(){
+    for (let i = 0; i < userArray.length; i++) {
+        let initials = userArray[i].initials;
+        let score = userArray[i].score;
 
-    let entry = document.createElement('li');
+        let entry = document.createElement('li');
 
-    entry.appendChild(document.createTextNode(initials + " " + score));
+        entry.appendChild(document.createTextNode(initials + " " + score));
 
-    listEl.appendChild(entry);
+        listEl.appendChild(entry);
+    }
 }
+
+function compare(b, a) {
+    const userA = a.score;
+    const userB = b.score;
+
+    let comparison = 0;
+    if (userA > userB) {
+        comparison = 1;
+    } else if (userA < userB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+userArray.sort(compare);
+
+printWinners();
+
